554ac23f19865890e31d960b4d12251affcec5c1
Tyrell Gordon <tygordon@microsoft.com>
2022-11-01 00:12:46 +0100

    Initial commit

---
+++ Source/MULTI/MULTI.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MULTI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MULTI, "MULTI" );
---
+++ Source/MULTI/MULTIGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MULTIGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTI_API AMULTIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
